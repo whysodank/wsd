065348e67ea5c4b334acb503499d03d4f4df6ec3
Işık Kaplan <isik.kaplan@outlook.com>
2025-04-22 17:20:53 +0300

    Fix signups lol

--- wsd/apps/user/models/user.py
+++ wsd/apps/user/models/user.py
@@ -44,7 +44,7 @@ class User(UserVoteMixin, UserBookmarkMixin, AbstractUser, BaseModel):
 
     @property
     def has_unusable_username(self):
-        return self.username.startswith(self.UNUSABLE_USERNAME_PREFIX)
+        return self.username and self.username.startswith(self.UNUSABLE_USERNAME_PREFIX)
 
     def save(self, *args, **kwargs):
         if self.has_unusable_username:
