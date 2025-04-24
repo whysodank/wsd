b37429dc81017d1da45b91222ca964fb6328b7c3
Işık Kaplan <isik.kaplan@outlook.com>
2025-04-24 12:12:20 +0300

    Admin cleanup

--- wsd/apps/user/admin/__init__.py
+++ wsd/apps/user/admin/__init__.py
@@ -2,10 +2,17 @@ from admin_interface.models import Theme
 from allauth.account.models import EmailAddress
 from allauth.socialaccount.models import SocialAccount, SocialApp, SocialToken
 from django.contrib import admin
+from django_celery_beat.models import ClockedSchedule, CrontabSchedule, IntervalSchedule, PeriodicTask, SolarSchedule
 
 from .group import GroupAdmin
 from .permission import PermissionAdmin
 from .user import UserAdmin
+
+admin.site.unregister(ClockedSchedule)
+admin.site.unregister(CrontabSchedule)
+admin.site.unregister(IntervalSchedule)
+admin.site.unregister(PeriodicTask)
+admin.site.unregister(SolarSchedule)
 
 admin.site.unregister(SocialApp)
 admin.site.unregister(SocialToken)
