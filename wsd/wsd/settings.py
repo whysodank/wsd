5aefb8a0fb6ab20fd6b8bbd3960068264066608f
Işık Kaplan <isik.kaplan@outlook.com>
2025-04-28 02:47:00 +0300

    People complained about too long password requirement

--- wsd/wsd/settings.py
+++ wsd/wsd/settings.py
@@ -144,7 +144,7 @@ DATABASES = {
 # Authentication
 AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"},
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", "OPTIONS": {"min_length": 16}},
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator", "OPTIONS": {"min_length": 8}},
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
