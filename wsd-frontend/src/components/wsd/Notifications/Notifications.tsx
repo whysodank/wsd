b6168aab871d10bc28f01f6043be8237a52f3140
Rooki <34943569+Pdzly@users.noreply.github.com>
2025-05-12 08:51:27 +0200

    Add "Mark all as read" feature for notifications (#21)
    * Add "Mark all as read" feature for notifications
    
    Introduce an API endpoint and frontend functionality to mark all notifications as read. Updated the backend view, API schema, and frontend components to support this feature, including notification state management and user feedback with toast messages.
    
    * Fixed all comments
    
    * Refactor notifications state update logic.
    
    Simplify and optimize the notifications state management by reducing redundant operations. Added logic to check for unread notifications while ensuring uniqueness in the notification list.

--- wsd-frontend/src/components/wsd/Notifications/Notifications.tsx
+++ wsd-frontend/src/components/wsd/Notifications/Notifications.tsx
@@ -18,6 +18,7 @@ import { APIType } from '@/api'
 import { useWSDAPI } from '@/lib/serverHooks'
 
 import { useInView } from 'react-intersection-observer'
+import { toast } from 'sonner'
 
 export function Notifications({ hasNew }: { hasNew?: boolean }) {
   const wsd = useWSDAPI()
@@ -26,6 +27,7 @@ export function Notifications({ hasNew }: { hasNew?: boolean }) {
   const [page, setPage] = useState(1)
   const [loading, setLoading] = useState(false)
   const [hasMore, setHasMore] = useState(true)
+  const [hasNewState, setHasNewState] = useState(hasNew)
 
   const { ref: loaderRef, inView } = useInView()
 
@@ -37,10 +39,10 @@ export function Notifications({ hasNew }: { hasNew?: boolean }) {
       ordering: '-created_at',
       include: 'post',
     })
-    setNotifications((prev) => {
-      const newNotifications = [...prev, ...(notificationsData?.results || [])]
-      return _.uniqBy(newNotifications, 'id')
-    })
+    const newNotifications = _.uniqBy([...notifications, ...(notificationsData?.results || [])], 'id')
+
+    setHasNewState(_.some(newNotifications, (notification) => !notification.is_read))
+    setNotifications(newNotifications)
     setHasMore(page !== notificationsData?.total_pages)
     setLoading(false)
   }
@@ -60,13 +62,40 @@ export function Notifications({ hasNew }: { hasNew?: boolean }) {
     }
   }
 
+  async function markAllAsRead() {
+    try {
+      const data = await wsd.markAllNotificationsAsRead()
+      const { error } = data
+      if (error) {
+        const errors = error as { detail: string }
+        toast(errors.detail, {
+          description: 'There was an error marking notifications as read.',
+        })
+        return
+      }
+      setNotifications((prev) => {
+        return prev.map((notification) => ({
+          ...notification,
+          is_read: true,
+        }))
+      })
+      toast(data.data?.message || 'Notifications marked as read')
+      setHasNewState(false)
+    } catch (error) {
+      console.error('Error marking notifications as read:', error)
+      toast('Error marking notifications as read', {
+        description: 'There was an error marking notifications as read.',
+      })
+    }
+  }
+
   return (
     <Overlay breakpoint="md" onOpenChange={onOverlayOpenChange}>
       <OverlayTrigger asChild>
         <Button variant="ghost" className="flex gap-2 h-10 w-10 rounded-full p-2">
           <div className="relative">
             <Icons.Bell size={20} />
-            {hasNew && <span className="absolute -top-0.5 -right-0.5 h-3 w-3 rounded-full bg-red-600 border" />}
+            {hasNewState && <span className="absolute -top-0.5 -right-0.5 h-3 w-3 rounded-full bg-red-600 border" />}
           </div>
         </Button>
       </OverlayTrigger>
@@ -74,6 +103,12 @@ export function Notifications({ hasNew }: { hasNew?: boolean }) {
         <OverlayTitle className="hidden">Notifications</OverlayTitle>
         <OverlayDescription className="hidden">Notifications</OverlayDescription>
         <ScrollArea className="max-h-[50vh] overflow-auto">
+          {!loading && hasNewState && (
+            <Button className="flex items-center gap-2 w-full" variant="outline" onClick={markAllAsRead} size={'sm'}>
+              <Icons.CheckCircle className="h-3 w-3" />
+              Mark all as read
+            </Button>
+          )}
           {notifications.map((notification) => (
             <div className="contents" key={notification.id}>
               <Notification notification={notification} />
