2d32bb80278714e242b235f4370151364119fac0
Pdzly <rooki@rooki.xyz>
2025-05-23 23:28:10 +0200

    Add PWA support with manifest, meta tags, and icons
    This update introduces Progressive Web App (PWA) support by adding a web app manifest with necessary metadata, including icons and descriptions. Relevant meta tags and links were also added to ensure compatibility with various platforms, enhancing the app's installability and appearance.

--- wsd-frontend/src/app/layout.tsx
+++ wsd-frontend/src/app/layout.tsx
@@ -26,6 +26,13 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover"
           name="viewport"
         />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#09090B" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="WSD" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" />
+        <title>{config.motto}</title>
         {gaID && <GoogleAnalytics gaId={gaID} />}
       </head>
       <body className="min-h-screen font-sans antialiased dark">
